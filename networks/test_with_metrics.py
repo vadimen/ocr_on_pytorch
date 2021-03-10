488f2d62239197e21c402a1db79529949386eb10
Vadim Placinta <vad1c111@gmail.com>
2021-03-10 06:58:10 +0200

    v1.3 added test

--- networks/test_with_metrics.py
+++ networks/test_with_metrics.py
@@ -22,12 +22,12 @@ if torch.cuda.is_available():
     torch.cuda.manual_seed(1)
     device = torch.device("cuda")
 
-character_set = "0123456789# "  # space is for nothing
+character_set = "-0123456789#"  # space is for nothing
 # create model
 model = model_list.ocrnet(device=device, num_classes=len(character_set))
 model.features = torch.nn.DataParallel(model.features)
 
-checkpoint = torch.load('../checkpoint_v1_1.pth.tar', map_location=torch.device('cpu'))
+checkpoint = torch.load('checkpoint.pth.tar', map_location=torch.device('cpu'))
 model.load_state_dict(checkpoint['state_dict'])
 model.to(device)
 
@@ -47,6 +47,8 @@ val_loader = torch.utils.data.DataLoader(
 def arr_to_label(arr):
     label = ''
     for c in arr:
+        if c == 100:
+            continue
         label = label + character_set[c]
     return label.strip()
 
@@ -64,20 +66,39 @@ def validate(val_loader, model):
     model.eval()
     all_percent_coincidence = 0
 
-    for i, (input, target) in enumerate(val_loader):
+    for i, (input, target, emb_len, target_len) in enumerate(val_loader):
         input_var = torch.autograd.Variable(input, volatile=True)
 
         # compute output
         start = time.time()
 
         output = model(input_var)
-        output = softmax(output.detach().cpu().numpy(), axis=2)
-        output = np.argmax(output, axis=2)
+        #output = softmax(output.detach().cpu().numpy(), axis=2)
+        output = torch.nn.functional.log_softmax(output, 2)
+        output = np.argmax(output.detach().cpu().numpy(), axis=2)
 
-        print(arr_to_label(output[0]))
-        print(arr_to_label(target.numpy()[0]))
+        label_from_model = arr_to_label(output[0])
 
-        prc = round(SQ(None, arr_to_label(target.numpy()[0]), arr_to_label(output[0])).ratio() * 100, 2)
+        #cut repeating symbols
+        res = ''
+        for c in label_from_model:
+            if len(res) == 0:
+                res = res + c
+            elif c != res[-1]:
+                res = res + c
+        label_from_model = res
+        res = ''
+        for c in label_from_model:
+            if c != '-':
+                res = res + c
+        label_from_model = res
+
+        label_true = arr_to_label(target.numpy()[0])
+
+        print(label_from_model)
+        print(label_true)
+
+        prc = round(SQ(None, label_from_model, label_true).ratio() * 100, 2)
         all_percent_coincidence = all_percent_coincidence + prc
         print(f"Percent coincidence: {prc}%")
 
